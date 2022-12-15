9e535937072f91744e29ae133be03e6ca3f49caa
Muhammad Usama <101458067+USAMI3@users.noreply.github.com>
2022-12-15 12:36:04 +0500

    initial commit by usama

--- lib/core/app/texts.dart
+++ lib/core/app/texts.dart
@@ -1,7 +1,7 @@
 class AppTexts {
   //login-texts
-  static const String loginHeading = 'Fossphorus';
-  static const String loginSubHeading = 'Welcome to Fossphorus';
+  static const String loginHeading = 'Ystop';
+  static const String loginSubHeading = 'Welcome to Ystop';
   static const String loginTextFieldHintEmail = 'Email';
   static const String loginTextFieldHintPassword = 'Password';
   static const String loginRemember = 'Remember me';
